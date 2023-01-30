d09364af77e5e8e93b34f01e9bf4b3980cf0d313
Mukul Sharma <mymukul112@gmail.com>
2023-01-30 23:00:48 +0530

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -12,12 +12,15 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
+# You must call it on initialization or listening to directory switching won't work
+load_nvm > /dev/stderr
+
 alias cl "clear"
-alias pp "cd ~/Work/personal-projects"
-alias p "cd ~/Work/project"
+alias pp "cd ~/work/personal-projects"
+alias p "cd ~/work/project"
 alias ho "cd ~"
 alias b "cd .."
-alias conf "cd ~/Work/personal-projects/config-file"
+alias conf "cd ~/work/personal-projects/.config"
 alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "ls -l"
@@ -26,3 +29,10 @@ alias g git
 alias doc "cd ~/Documents"
 alias dow "cd ~/Downloads"
 alias app "cd ~/Application"
+alias learn "cd ~/work/learn"
+alias key "cd ~/Documents/keys"
+alias update "sudo dnf update --exclude=brave-browser,firefox,code"
+alias update-all "sudo dnf update"
+alias pas "php artisan serve"
+alias yrs "yarn run start"
+alias nrs "npm run start"
