d47a23e1062c0c19cb2fa9e03fe3886dd70d8927
Mukul Sharma <mymukul112@gmail.com>
2023-03-11 11:18:08 +0530

    replace config with lazyvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,2 @@
-require('mukul98s.base')
-require('mukul98s.highlights')
-require('mukul98s.maps')
-require('mukul98s.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-vim.cmd [[colorscheme dracula]]
-
-if is_mac then
-  require('mukul98s.macos')
-end
-if is_win then
-  require('mukul98s.windows')
-end
-if is_wsl then
-  require('mukul98s.wsl')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
