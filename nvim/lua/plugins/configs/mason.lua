04761dc1a03c6aaa2f72a4cecb3b1f7c55c6ad57
Mukul Sharma <mymukul112@gmail.com>
2024-02-24 18:12:35 +0530

    Nvim config update

--- nvim/lua/plugins/configs/mason.lua
+++ nvim/lua/plugins/configs/mason.lua
@@ -1,5 +1,8 @@
 local options = {
-  ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
+  ensure_installed = {
+    "lua-language-server",
+    "gopls"
+  }, -- not an option from mason.nvim
 
   PATH = "skip",
 
